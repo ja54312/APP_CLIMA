8c4c241da162d1706ef0cc10250904b4e3c63450
ja54312 <ija54312@gmail.com>
2022-08-28 12:58:31 -0500

    version base

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App(){
+    return(
+      <>
+          <h2>App Clima</h2>
+      </>
+    )
+  }
+  
+  const entryPointMalta = document.getElementById("root");
+  ReactDOM.render(<App/>, entryPointMalta);
